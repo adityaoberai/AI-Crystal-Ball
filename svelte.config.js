710520c01ae997a88a579c8ad0b517ffe5b60d83
adityaoberai <adityaoberai1@gmail.com>
2024-04-18 18:43:29 +0530

    Update max duration for vercel function

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-vercel';
+ 
+// This function can run for a maximum of 5 seconds
+export default {
+  kit: {
+    adapter: adapter({
+      maxDuration: 60,
+    }),
+  },
 };
-
-export default config;
