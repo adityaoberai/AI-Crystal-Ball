2c808b8d5205bcc975713e634c5c7948ed681663
adityaoberai <adityaoberai1@gmail.com>
2024-04-22 20:37:51 +0530

    Update Appwrite endpoint

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, Storage } from 'appwrite';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint('https://appwritecloud.oberai.dev/v1')
     .setProject('aicrystalball');
 
 export const account = new Account(client);
--- src/routes/destiny/[slug]/+page.server.js
+++ src/routes/destiny/[slug]/+page.server.js
@@ -1,5 +1,8 @@
 import { db } from '$lib/databases';
 
+export const ssr = true;
+export const csr = false;
+
 export async function load({ params }) {
     let destiny = await db.getDestiny(params.slug);
     
